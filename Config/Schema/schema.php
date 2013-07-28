97c3c409b96a8105f0cdd029173d985df3863583
Angel S. Moreno <angelxmoreno@gmail.com>
2013-07-28 06:25:43 -0400

    Added schema.php file created my cakephp migrations

---
+++ Config/Schema/schema.php
@@ -0,0 +1,75 @@
+<?php 
+class AppSchema extends CakeSchema {
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $groups = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'description' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'privacy' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 100, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'members_count' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 10),
+		'icon' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'admin_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'index'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'admin_id' => array('column' => 'admin_id', 'unique' => 0),
+			'privacy' => array('column' => 'privacy', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+	public $groups_users = array(
+		'group_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'user_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'index'),
+		'indexes' => array(
+			'group_id' => array('column' => 'group_id', 'unique' => 0),
+			'user_id' => array('column' => 'user_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+	public $posts = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'group_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'index'),
+		'from_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'index'),
+		'to_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'index'),
+		'message' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'likes_count' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 10),
+		'comments_count' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 10),
+		'parent_id' => array('type' => 'biginteger', 'null' => true, 'default' => null, 'key' => 'index'),
+		'post_date' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'from_id' => array('column' => 'from_id', 'unique' => 0),
+			'to_id' => array('column' => 'to_id', 'unique' => 0),
+			'parent_id' => array('column' => 'parent_id', 'unique' => 0),
+			'group_id' => array('column' => 'group_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+	public $users = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 200, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'access_token' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'token_expires' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'pic_url' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+}
